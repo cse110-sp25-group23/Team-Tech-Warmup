8ebf5235ef243a92c7c40887db623b0370557dcb
SarahChun6 <sarahchun66@gmail.com>
2025-04-19 13:11:49 -0700

    Added shuffle animation, need to work on playing-card web component visuals

--- card-deck.js
+++ card-deck.js
@@ -1,30 +1,46 @@
-/**
- * card suits and values
- */
-const suits = ["Diamond", "Spade", "Club", "Heart"];
-const card_values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A",]; 
+
+
+class PlayingCard extends HTMLElement {
+    constructor(){
+        super();
+    }
+    /*connectedCallback() {
+        // Get attributes
+        const suit = this.getAttribute("suit") || "placeholder";
+        const number = this.getAttribute("number") || "0";
+        //const imageSrc = this.getAttribute("image") || "placeholder.jpg";
+        this.innerHTML = `
+            <h2>${number}${suit}</h2>
+            <!--<img src="${imageSrc}" alt="Screenshot of ${name}" width="200" height= "150">-->
+        `;
+    }*/
+}
+
+// Define the custom element
+customElements.define("playing-card", PlayingCard);
 
 class CardDeck extends HTMLElement {
     connectedCallback() {
-        // Get attributes
-        // const name = this.getAttribute("name") || "Card Deck"; //is this needed?
-        const imageSrc = this.getAttribute("image") || "placeholder.jpg"; //picture of back of card or of facedown stack 
         this.innerHTML = `
             <section class="deck">
                 <h2>Card Deck</h2>
-                <div class="cards">
-                </div>
+                    <button name="shuffle">Shuffle Deck</button>
+                    <img src="card_deck.jpg" alt="Top of facedown card deck" width="620" height="370">
+                <div class="cards"></div>
             </section> 
         `;
 
         const cardContainer = this.querySelector(".cards");
+
+        const suits = ["Diamond", "Spade", "Club", "Heart"];
+        const card_values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A",]; 
 
         for (let i = 0; i < suits.length; ++i) {
             for(let j = 0; j < card_values.length; ++j) {
                 const cardElement = document.createElement("playing-card");
                 cardElement.setAttribute("suit", suits[i]);
                 cardElement.setAttribute("number", card_values[j]);
-                cardContainer.appendChild(cardElement);
+                cardContainer.appendChild(cardElement); //this
                 /*
                 //Construct an image URL based on the card (adjust for your asset path)
                 const imgName = `${card.Value}_of_${card.Suit}s`.toLowerCase().replace(/ /g, "_");
@@ -33,8 +49,22 @@ class CardDeck extends HTMLElement {
                 */
             }
         }
+    
+        const shuffleButton = this.querySelector('button[name="shuffle"]');
+        const deckImage = this.querySelector('img');
+
+        shuffleButton.addEventListener('click', () => {
+            console.log("shuffled deck");
+
+            deckImage.src = "https://miro.medium.com/v2/resize:fit:828/format:webp/1*3l1h9uWiNRggMXH-ibcVVA.gif";
+
+            setTimeout(() => { //reset to original image after gif is done
+                deckImage.src = "card_deck.jpg";
+            }, 2000);
+        });
 
     }
+
 }
 
 // Define the custom element
